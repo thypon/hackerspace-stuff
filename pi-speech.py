1d6473c0e0d3c15946c19cd1e15c7d1407c4daf2
Edoardo Negri <edne@gmx.com>
2015-05-11 16:59:21 +0200

    pi speech

---
+++ pi-speech.py
@@ -0,0 +1,19 @@
+from sh import espeak
+from time import sleep
+
+
+def pi_digits():
+    q, r, t, k, m, x = 1, 0, 1, 1, 3, 3
+    while True:
+        if 4 * q + r - t < m * t:
+            yield m
+            q, r, t, k, m, x = (10*q, 10*(r-m*t), t, k,
+                                (10*(3*q+r))//t - 10*m, x)
+        else:
+            q, r, t, k, m, x = (q*k, (2*q+r)*x, t*x,
+                                k+1, (q*(7*k+2)+r*x)//(t*x), x+2)
+
+
+for digit in pi_digits():
+    espeak(digit)
+    sleep(1)
